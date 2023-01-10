1d5a141c9448865bbb5bca70452b491dd5a38a2f
onojaonoja2 <samuelonoja06@gmail.com>
2023-01-10 13:17:01 -0800

    Completed task

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,7 @@
-# manifest to create file in tmp directory
+# Creates a file
 file { '/tmp/school':
-  ensure  => file,
-  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet',
+  content => 'I love Puppet'
+}
